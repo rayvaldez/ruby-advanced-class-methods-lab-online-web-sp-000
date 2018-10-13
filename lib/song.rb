a1143941e37303b5bbf196a725f8bff3b417fbea
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-10-13 12:10:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
     song
   end
 
-  def self.find_by_name(song_string)
-    @@all.find{|song| song.song_string == song_string}
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
   end
 end
