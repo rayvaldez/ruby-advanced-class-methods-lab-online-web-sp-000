ff475350ca3e199ed2c73c224b6042c8d7164333
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-10-13 12:19:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
 
   def self.create_from_filename(name)
     song = self.new
-    song.name = (name.sokut(" - ")[1].chomp(".mp3"))
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0])
     @@all << song
     song
